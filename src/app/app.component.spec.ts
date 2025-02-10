90731b7249bf9a0c2771e7fd91da38b007603fd6
Alexandr Kalyan <alexandrkalyan009@gmail.com>
2025-02-10 15:34:27 +0200

    Updated navigation bar and styles for it.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,6 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-orbistay-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-orbistay-frontend');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import {BehaviorSubject} from 'rxjs';
+import { Component, HostListener } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,34 +9,66 @@ import {BehaviorSubject} from 'rxjs';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-orbistay-frontend';
+  currentUser: any = null;
+  isPopupVisible: boolean = false;
+  firstLetter$ = new BehaviorSubject<string>('');
+  constructor(public router: Router) {}
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.popup-menu') && !target.closest('.user-button')) {
+      this.isPopupVisible = false;
+    }
+  }
+
+  getFirstLetter(): string {
+    return this.currentUser?.name?.charAt(0).toUpperCase() || '';
+  }
+
+  togglePopup(event: Event) {
+    event.stopPropagation();
+    this.isPopupVisible = !this.isPopupVisible;
+  }
+
+  signOut() {
+    this.currentUser = null;
+    localStorage.removeItem('userData');
+    localStorage.removeItem('token');
+    this.currentUserSubject.next(this.currentUser);
+    this.updateUserData(this.currentUser);
+    this.isPopupVisible = !this.isPopupVisible;
+    window.location.reload();
+  }
+
   private currentUserSubject = new BehaviorSubject<any>(null);
-  public currentUser = this.currentUserSubject.asObservable();
-  private isLoggedIn = false;
+  public currentUser$ = this.currentUserSubject.asObservable();
 
   ngOnInit(): void {
     const storedUser = localStorage.getItem('userData');
     if (storedUser) {
       this.currentUser = JSON.parse(storedUser);
       this.currentUserSubject.next(this.currentUser);
+      this.firstLetter$.next(this.getFirstLetter());
     } else {
       console.log('No user data found in localStorage');
     }
 
-    this.currentUser.subscribe(user => {
+    this.currentUser$.subscribe(user => {
       this.currentUser = user;
+      this.firstLetter$.next(this.getFirstLetter());
     });
-
-    console.log(this.currentUser);
   }
 
   updateUserData(userData: any) {
     this.currentUser = userData;
     this.currentUserSubject.next(this.currentUser);
     localStorage.setItem('userData', JSON.stringify(userData));
+
+    this.firstLetter$.next(this.getFirstLetter());
   }
 
   getLoginStatus() {
-    return this.isLoggedIn;
+    return this.currentUser != null;
   }
 }
