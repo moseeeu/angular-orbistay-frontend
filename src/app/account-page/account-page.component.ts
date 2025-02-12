60cb1d8061eabbddde78d3a2aded06737ddc32a2
Alexandr Kalyan <alexandrkalyan009@gmail.com>
2025-02-12 16:25:02 +0200

    Fixed passport edit fields style and simplify logic.

--- src/app/account-page/account-page.component.ts
+++ src/app/account-page/account-page.component.ts
@@ -5,6 +5,7 @@ import {BehaviorSubject} from 'rxjs';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {ApiUrls} from '../api-urls';
 import {AuthService} from '../auth.service';
+import {NgxSpinnerService} from 'ngx-spinner';
 
 @Component({
   selector: 'app-account-page',
@@ -16,20 +17,12 @@ import {AuthService} from '../auth.service';
 export class AccountPageComponent {
   @ViewChild('fileInput') fileInput!: ElementRef;
   currentUser: any;
+  userFields: any[] = [];
   activeContent: string = 'content1';
   firstLetter: string = '';
   private currentUserSubject = new BehaviorSubject<any>(null);
-  constructor(public appComponent: AppComponent, public router: Router, public http: HttpClient, private auth: AuthService) { }
-
-  username: string | undefined = '';
-  displayName: string | undefined = '';
-  email: string | undefined  = '';
-  phone: string | undefined = '';
-  birthDate: string | undefined = 'Enter your date of birth';
-  nationality: string | undefined = 'Select the country/region you are from';
-  gender: string | undefined = 'Select your gender';
-  address: string | undefined = 'Add your address';
-  passportDetails: string | undefined = 'Not provided';
+  constructor(public router: Router, public http: HttpClient, private auth: AuthService,private spinner: NgxSpinnerService) { }
+  avatar: string | undefined = '';
 
   passportData = {
     name: '',
@@ -41,7 +34,6 @@ export class AccountPageComponent {
     agree: false
   };
   name: string = "";
-  surname: string = "";
   countries: any[] = [];
   selectedCountry: string = 'Ukraine';
   selectedMonth: string = '01';
@@ -60,35 +52,60 @@ export class AccountPageComponent {
     { name: '12-Dec', code: '12' },
   ];
 
-  userFields = [
-    { label: 'Name', value: this.username, type: '', verified: false, description: '', editing: false },
-    { label: 'Display name', value: this.displayName, type: '', verified: false, description: '', editing: false },
-    { label: 'Email address', value: this.email, type: 'mail', verified: true, description: 'This is the email address you use to sign in. It’s also where we send your booking confirmation.', editing: false },
-    { label: 'Phone number', value: this.phone, type: 'mail', verified: false, description: 'Properties or attractions you book will use this number if they need to contact you.', editing: false },
-    { label: 'Date of birth', value: this.birthDate, type: '', verified: false, description: '', editing: false },
-    { label: 'Nationality', value: this.nationality, type: '', verified: false, description: '', editing: false },
-    { label: 'Gender', value: this.gender, type: '', verified: false, description: '', editing: false },
-    { label: 'Address', value: this.address, type: '', verified: false, description: '', editing: false },
-    { label: 'Passport details', value: this.passportDetails, type: '', verified: false, description: '', editing: false }
-  ];
   preferences = [
     { label: 'Currency', value: 'UAH Ukraine Hryvnia', editing: false },
     { label: 'Language', value: 'American English', editing: false }
   ];
 
   saveField(field: any) {
-    let updatedData;
-    const token = localStorage.getItem('token');
-    console.log("Token",token)
+    const fieldMap: { [key: string]: string } = {
+      'Display name': 'username',
+      'Email address': 'email',
+      'Phone number': 'phone',
+      'Date of birth': 'birthDate',
+      'Nationality': 'citizenship',
+      'Gender': 'gender',
+      'Address': 'residency.street',
+      'Passport details': 'passport'
+    };
 
-    if (field.label === 'Passport details') {
-      updatedData = {
-        passportData: this.passportData
-      };
+    const token = localStorage.getItem('token');
+    console.log("Token", token);
+
+    const userFieldKey = fieldMap[field.label];
+
+    if (!userFieldKey) {
+      console.error('Unknown field label:', field.label);
+      return;
+    }
+
+    const updatedData: any = {
+      username: this.currentUser.username,
+      email: this.currentUser.email,
+      phone: this.currentUser.phone,
+      birthDate: this.currentUser.birthDate,
+      citizenship: this.currentUser.citizenship,
+      gender: this.currentUser.gender,
+      residency: {
+        street: this.currentUser.residency?.street || '',
+        city: this.currentUser.residency?.city || ''
+      },
+      passport: {
+        firstName: this.currentUser.passport?.firstName || '',
+        lastName: this.currentUser.passport?.lastName || '',
+        passportNumber: this.currentUser.passport?.passportNumber || '',
+        countryIssuedCode: this.currentUser.passport?.countryIssuedCode || '',
+        expirationDate: this.currentUser.passport?.expirationDate || ''
+      }
+    };
+
+    if (userFieldKey.startsWith('residency.')) {
+      const key = userFieldKey.split('.')[1];
+      updatedData.residency[key] = field.value;
+    } else if (userFieldKey === 'passport') {
+      updatedData.passport = field.value;
     } else {
-      updatedData = {
-        [field.label]: field.value
-      };
+      updatedData[userFieldKey] = field.value;
     }
 
     const headers = new HttpHeaders({
@@ -140,18 +157,89 @@ export class AccountPageComponent {
   }
 
   ngOnInit() {
-    this.getCountriesList()
-    if (!this.auth.hasToken()) { this.router.navigate(['/login']); }
+    this.getCountriesList();
+    if (!this.auth.hasToken()) {
+      this.router.navigate(['/login']);
+      return;
+    }
     const storedUser = localStorage.getItem('userData');
     if (storedUser) {
       this.currentUser = JSON.parse(storedUser);
-      this.currentUserSubject.next(this.currentUser);
-      this.firstLetter = this.currentUser.username[0].toUpperCase();
-      console.log("user info", this.currentUser);
-    }
-    else {
+      this.userFields = this.generateUserFields(this.currentUser);
+      this.avatar = this.currentUser.avatarUrl;
+      console.log("User info", this.currentUser);
+    } else {
       console.log('No user data found in localStorage');
     }
+  }
+
+  generateUserFields(user: any): any[] {
+    return [
+      {
+        label: 'Display name',
+        value: user.username || 'Enter your display name',
+        type: '',
+        verified: false,
+        description: '',
+        editing: false
+      },
+      {
+        label: 'Email address',
+        value: user.email || 'Enter your email address',
+        type: 'mail',
+        verified: true,
+        description: 'This is the email address you use to sign in. It’s also where we send your booking confirmation.',
+        editing: false
+      },
+      {
+        label: 'Phone number',
+        value: user.phone || 'Not provided',
+        type: 'phone',
+        verified: false,
+        description: 'Properties or attractions you book will use this number if they need to contact you.',
+        editing: false
+      },
+      {
+        label: 'Date of birth',
+        value: user.birthDate || 'Enter your date of birth',
+        type: 'date',
+        verified: false,
+        description: '',
+        editing: false
+      },
+      {
+        label: 'Nationality',
+        value: user.citizenship || 'Select the country/region you are from',
+        type: '',
+        verified: false,
+        description: '',
+        editing: false
+      },
+      {
+        label: 'Gender',
+        value: user.gender || 'Select your gender',
+        type: '',
+        verified: false,
+        description: '',
+        editing: false
+      },
+      {
+        label: 'Address',
+        value: user.address || 'Add your address',
+        type: '',
+        verified: false,
+        description: '',
+        editing: false
+      },
+      {
+        label: 'Passport details',
+        value: user.passportDetails || 'Not provided',
+        type: '',
+        verified: false,
+        description: '',
+        editing: false
+      }
+    ];
   }
 
   getCountriesList() {
@@ -176,12 +264,21 @@ export class AccountPageComponent {
   openFileDialog() {
     this.fileInput.nativeElement.click();
   }
-
+  showLoadingScreen() {
+  }
   onFileSelected(event: Event) {
     const target = event.target as HTMLInputElement;
     if (target.files && target.files.length > 0) {
       const selectedFile = target.files[0];
-      console.log('Выбран файл:', selectedFile.name);
+      console.log("ava", selectedFile);
+      this.auth.updateUserAvatar(selectedFile).subscribe((response) => {
+        console.log(response);
+      });
+      this.spinner.show();
+      setTimeout(() => {
+        this.spinner.hide();
+        window.location.reload();
+      }, 5000);
     }
   }
 }
