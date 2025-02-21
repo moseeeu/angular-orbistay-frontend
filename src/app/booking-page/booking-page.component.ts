b43207a0081a8a41276c8a5115235a1ba76ab8a9
Alexandr Kalyan <alexandrkalyan009@gmail.com>
2025-02-21 17:49:24 +0200

    Added a test markup for hotel booking page

---
+++ src/app/booking-page/booking-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-page',
+  standalone: false,
+  
+  templateUrl: './booking-page.component.html',
+  styleUrl: './booking-page.component.css'
+})
+export class BookingPageComponent {
+
+}
