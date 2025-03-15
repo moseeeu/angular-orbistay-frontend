a3702191905d13291abc6bc523355284c587d715
Alexandr Kalyan <alexandrkalyan009@gmail.com>
2025-03-15 19:11:02 +0200

    Fixed rating validation and styles in hotel card

--- src/app/hotel-view-card/hotel-view-card.component.ts
+++ src/app/hotel-view-card/hotel-view-card.component.ts
@@ -13,4 +13,26 @@ export class HotelViewCardComponent {
   @Input() rating: number = 0;
   @Input() reviews: number = 0;
   @Input() image: string = '';
+
+  hotelGrade: any;
+
+  ngOnInit() {
+    switch (true) {
+      case (this.rating === 0):
+        this.hotelGrade = "No rating";
+        break;
+      case (this.rating < 6):
+        this.hotelGrade = "Value stay";
+        break;
+      case (this.rating < 8 && this.rating >= 6):
+        this.hotelGrade = "Good";
+        break;
+      case (this.rating < 9 && this.rating >= 8):
+        this.hotelGrade = "Very good";
+        break;
+      case (this.rating <= 10 && this.rating >= 9):
+        this.hotelGrade = "Excellent";
+        break;
+    }
+  }
 }
