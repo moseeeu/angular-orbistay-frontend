3aa97258660d426c852d93764994622c6b281d82
Alexandr Kalyan <alexandrkalyan009@gmail.com>
2025-03-15 19:14:11 +0200

    Fixed logic and markup in room modal window

--- src/app/hotel-room-modal-page/hotel-room-modal-page.component.ts
+++ src/app/hotel-room-modal-page/hotel-room-modal-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-hotel-room-modal-page',
@@ -9,20 +9,24 @@ import {Component, EventEmitter, Output} from '@angular/core';
 })
 export class HotelRoomModalPageComponent {
   @Output() close = new EventEmitter<void>();
+  @Input() room: any;
+  currentImageIndex: number = 0;
+  currentIndex: number = 0;
+  reviewsPerPage: number = 3;
 
   room_facilities = [
     { name: 'Air Conditioning', icon: 'hotel-facilities/air-conditioning.svg' },
     { name: 'Antique Furniture', icon: 'hotel-facilities/antique-furniture.svg' },
-    { name: 'Beautiful View', icon: 'hotel-facilities/beautiful-view.svg' },
-    { name: 'Flatscreen TV', icon: 'hotel-facilities/flatscreen-tv.svg' },
+    { name: 'City View', icon: 'hotel-facilities/beautiful-view.svg' },
+    { name: 'Flat-Screen TV', icon: 'hotel-facilities/flatscreen-tv.svg' },
     { name: 'Free Parking', icon: 'hotel-facilities/free-parking.svg' },
-    { name: 'Free Wi-Fi', icon: 'hotel-facilities/free-wi-fi.svg' },
+    { name: 'Free WiFi', icon: 'hotel-facilities/free-wi-fi.svg' },
     { name: 'Hair Dryer', icon: 'hotel-facilities/hair-dryer.svg' },
     { name: 'Iron', icon: 'hotel-facilities/iron.svg' },
     { name: 'King-sized Bed', icon: 'hotel-facilities/king-sized-bed.svg' },
     { name: 'Mini Bar', icon: 'hotel-facilities/mini-bar.svg' },
     { name: 'Modern Amenities', icon: 'hotel-facilities/modern-amenities.svg' },
-    { name: 'Private Balcony', icon: 'hotel-facilities/private-balcony.svg' },
+    { name: 'Balcony', icon: 'hotel-facilities/private-balcony.svg' },
     { name: 'Single Bed', icon: 'hotel-facilities/single-bed.svg' },
     { name: 'Telephone', icon: 'hotel-facilities/telephone.svg' },
     { name: 'Work Desk', icon: 'hotel-facilities/work-desk.svg' },
@@ -47,6 +51,26 @@ export class HotelRoomModalPageComponent {
     "Slippers",
     "Wardrobe",
   ]
+
+  ngOnInit() {
+    console.log("Room on modal page", this.room);
+  }
+
+  changeImage(room: any, direction: number) {
+    const totalImages = room.images.length;
+    this.currentImageIndex = (this.currentImageIndex + direction + totalImages) % totalImages;
+  }
+
+  checkFacility(roomFacilities: any[], facilityName: string): boolean {
+    return roomFacilities.some(facility => facility.name === facilityName);
+  }
+  getRoomViews(): string[] {
+    return this.room.roomFacilityLinks
+      .filter((facility: { name: string; }) => facility.name.toLowerCase().includes('view'))
+      .map((facility: { name: any; }) => facility.name);
+  }
+
+
 
   closeModal() {
     this.close.emit();
